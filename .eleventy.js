52eff62bb9cc0aae1df30a13028a55d0d378537c
TheAtu <alfonsundblad@gmail.com>
2021-10-05 22:57:09 -0300

    first commit - main site build

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const{ DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+  //Watch for changes, if(change==>recompile)
+    eleventyConfig.addWatchTarget("./src/scss");
+    eleventyConfig.addWatchTarget("*njk");
+
+  // Include other assets/items in public
+    eleventyConfig.addPassthroughCopy('./src/assets');
+  //Include Css compiled files
+    eleventyConfig.addPassthroughCopy("./src/css")
+
+  //Use standard time luxon to change default dates to "date_Med"
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    })
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  }
+};
--- src/blog/2021-05-01-my-first-article.md
+++ src/blog/2021-05-01-my-first-article.md
@@ -1,10 +1,10 @@
 ---
-title: My First Article
-author: Kevin Powell
+title: Keno es gay, not news
+author: Atu Sundblad
 date: 2021-05-01
 tags: ["post", "featured"]
 image: /assets/blog/article-1.jpg
-imageAlt: This is a test
+imageAlt: A good image provided by stock internet
 description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis accusantium sit illo neque rem omnis quaerat, nam similique vitae delectus ad magni vel quo maxime, magnam placeat. Reprehenderit, distinctio aliquam?
 ---
 
